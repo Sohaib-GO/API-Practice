f65b70279ed2d4f35bd78d7dbfe777086038ef25
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-24 00:49:31 -0700

    Refactored code + added tests for the functions

--- json_the_cat/breedFetcher.js
+++ json_the_cat/breedFetcher.js
@@ -1,28 +1,23 @@
 const request = require("request");
-const arg = process.argv[2];
 
 const fetchBreedDescription = (breedName, callback) => {
   request(
     `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
     (error, response, body) => {
       if (error) {
-        callback(error);
+        callback(error , null);
         return;
       } else {
         const data = JSON.parse(body);
         if (data.length === 0) {
-          callback("Breed not found");
+          callback("Breed not found", null);
           return;
         } else {
-          console.log(data[0].description);
+          callback(null, data[0].description);
         }
       }
     }
   );
 };
 
-fetchBreedDescription(arg, (error) => {
-  if (error) {
-    console.log("Error fetch details:", error);
-  }
-});
+module.exports = { fetchBreedDescription };
