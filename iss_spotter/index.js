de816189459f38a628db10ad3bad967d65f31bc2
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-24 19:04:51 -0700

    functions chaning to promise

--- iss_spotter/index.js
+++ iss_spotter/index.js
@@ -1,24 +1,24 @@
-const { nextISSTimesForMyLocation } = require("./iss");
+// const { nextISSTimesForMyLocation } = require("./iss");
 
-// fetchCoordsByIP('50.65.122.169', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+// // fetchCoordsByIP('50.65.122.169', (error, coordinates) => {
+// //   if (error) {
+// //     console.log("It didn't work!" , error);
+// //     return;
+// //   }
 
-//   console.log('It worked! Returned coordinates:' , coordinates);
-// });
+// //   console.log('It worked! Returned coordinates:' , coordinates);
+// // });
 
-// const exampleCoords = { latitude: 53.5412083, longitude: -113.2957365 };
+// // const exampleCoords = { latitude: 53.5412083, longitude: -113.2957365 };
 
-// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+// // fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+// //   if (error) {
+// //     console.log("It didn't work!" , error);
+// //     return;
+// //   }
 
-//   console.log('It worked! Returned flyover times:' , passTimes);
-// });
+// //   console.log('It worked! Returned flyover times:' , passTimes);
+// // });
 
 const printPassTimes =  (passTimes) => {
   for (const pass of passTimes) {
@@ -29,9 +29,12 @@ const printPassTimes =  (passTimes) => {
   }
 };
 
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  printPassTimes(passTimes);
-});
+// nextISSTimesForMyLocation((error, passTimes) => {
+//   if (error) {
+//     return console.log("It didn't work!", error);
+//   }
+//   printPassTimes(passTimes);
+// });
+
+
+module.exports = {printPassTimes};
